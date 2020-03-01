6d7de423ba17bda86881c6895d6d29ac514a7ead
Kendal Montgomery <kmontgomery@cbuscollaboratory.com>
2020-03-01 14:47:42 +0000

    fixed groups routing

--- fitness-elite-site/fitness-elite/src/modules/index-module/index-module.module.ts
+++ fitness-elite-site/fitness-elite/src/modules/index-module/index-module.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { IndexModuleComponent } from './index-module.component';
 import { FitnessEliteCommonModule } from '../FitnessEliteCommon/FitnessEliteCommon.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
     FitnessEliteCommonModule
   ],
   declarations: [
